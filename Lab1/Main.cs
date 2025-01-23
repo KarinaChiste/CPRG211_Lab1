602e8c9ff59d23294886b82e984b6978af8c264f
KarinaChiste <163331375+KarinaChiste@users.noreply.github.com>
2025-01-22 19:39:59 -0700

    Finished Person Class

---
+++ Lab1/Main.cs
@@ -0,0 +1,10 @@
+﻿using Lab1;
+
+Person person1 = new Person(1, "Ian", "Brooks", "Red", 30, true);
+Person person2 = new Person(2, "Gina", "James", "Green", 18, false);
+Person person3 = new Person(3, "Mike", "Briscoe", "Blue", 45, true);
+Person person4 = new Person(4, "Mary", "Beals", "Yellow", 28, true);
+
+Console.WriteLine(person1.DisplayPersonInfo());
+Console.WriteLine(person1);
+    
---
+++ Lab1/Person.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class Person
+    {
+        //private data
+        private int personId;
+        private string firstName;
+        private string lastName;
+        private string favouriteColour;
+        private int age;
+        private bool isWorking;
+
+        //public properties
+        public string FavouriteColour 
+            {
+            get { return favouriteColour; }
+            set {favouriteColour = value; }
+
+            }
+        //constructor
+        public Person(int personId, string firstName, string lastName,string favouriteColour, int age, bool isWorking)
+        {
+            this.personId = personId;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.FavouriteColour = favouriteColour;
+            this.age = age;
+            this.isWorking = isWorking;
+        }
+
+        // methods
+        public string DisplayPersonInfo()
+        {
+            return $"{personId}: {firstName} {lastName}'s favourite colour is {favouriteColour}";
+        }
+
+        public string ChangeFavouriteColour()
+        {
+            FavouriteColour = "White";
+            return FavouriteColour;
+        }
+        
+        public int GetAgeInTenYears()
+        {
+            return age + 10;
+        }
+
+        public override string ToString() 
+        {
+            return $"PersonID: {personId} \nFirstName: {firstName} \nLastName: {lastName} \nFavouriteColour: {FavouriteColour} \nAge: {age} \nIsWorking{isWorking}";
+        }
+
+    } //class
+}
